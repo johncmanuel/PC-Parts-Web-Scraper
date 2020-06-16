a32806e664c50675374002a61b695a5977b9b38c
John Carlo Manuel <jmanuel51801@gmail.com>
2020-06-16 09:00:53 -0700

    Refactoring comments and code

--- pipelines.py
+++ pipelines.py
@@ -10,14 +10,14 @@ class PerCategoryJsonExportPipeline:
 
     @classmethod
     def from_crawler(cls, crawler):
-        """ Get spider's name """
+        """ Get spider's name. """
         return cls(crawler.spider.name)
 
     def __init__(self, spider_name):
         self.spider_name = spider_name
 
     def open_spider(self, spider):
-        """ Create a dictionary for product_category in object Item"""
+        """ Create a dictionary for product_category in object Item. """
         self.category_to_exporter = dict()
 
     def close_spider(self, spider):
@@ -25,14 +25,14 @@ class PerCategoryJsonExportPipeline:
             exporter.finish_exporting()
 
     def _exporter_for_item(self, item):
-        """ Sort scraped data according to their spider and category """
+        """ Sort scraped data according to their spider and category. """
         category = item['product_category']
         dir = 'data/output/{0}'.format(self.spider_name)
 
-        """ Create a directory if it doesn't exist """
+        """ Create a directory if it doesn't exist. """
         Path(dir).mkdir(parents=True, exist_ok=True)
 
-        """ Check if category exists in dictionary """
+        """ Check if category exists in dictionary. """
         if category not in self.category_to_exporter:
             """ Export scraped data into a json file with their spider name and category. """
             f = open('data/output/{0}/{1}_{2}.json'.format(self.spider_name, self.spider_name, category), 'wb')
