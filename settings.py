b49d3318cd0fd4df470b151ff2b4ba51039d42d9
John Carlo Manuel <jmanuel51801@gmail.com>
2020-06-18 14:55:59 -0700

    Removed unnecessary comment

--- settings.py
+++ settings.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for pcparts project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = 'pcparts'
 
 SPIDER_MODULES = ['pcparts.spiders']
